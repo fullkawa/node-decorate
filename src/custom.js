cada49986e32ab255fffe84a64fdf3702756c72e
lex <lex.and.msol@gmail.com>
2012-09-19 14:48:34 +0900

    Comment out: sample codes

--- src/custom.js
+++ src/custom.js
@@ -1,9 +1,13 @@
 module.exports = {
+	/* example
 	append_manipulate : ['hello']
+	*/
 }
 
 module.exports.proc_for_elements = {
+	/* example
 	hello : function($) {
 		$('body').attr('onload', 'alert("Hello, world !")');
 	}
+	*/
 };
